3317cbdb317f38033e2fd45bd72462a994f5ca9f
mwaura.gerald <mwaura.gerald@nordcloud.com>
2022-03-11 21:25:02 +0200

    check config path

--- profile/providers.tf
+++ profile/providers.tf
@@ -10,5 +10,5 @@ provider "kubernetes" {
     host                   = "https://${module.gke.endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-    config_path    = "~/.kube/config"
+    //config_path    = "~/.kube/config"
 }
